5a4faf3ef56e663d7b36bc2608f3f6405f03709d
Oscar Vallejo <oscarvallejoipuz@gmail.com>
2023-09-10 16:51:38 -0500

    MoviesApiNode

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,15 @@
-const express = require('express');
+//index.js
+const express = require("express");
+const actorsRouter = require("./actors.router");
+const directorsRouter = require("./directors.router");
+const genresRouter = require("./genres.router");
+const moviesRouter = require("./movies.router");
 const router = express.Router();
 
-// colocar las rutas aquí
-
+// Configura las rutas para actores, directores, géneros y películas
+router.use("/actors", actorsRouter);
+router.use("/directors", directorsRouter);
+router.use("/genres", genresRouter);
+router.use("/movies", moviesRouter);
 
 module.exports = router;
